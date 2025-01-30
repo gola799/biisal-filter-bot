077e5106e9e6ff8595cb9228fd255b7dc9d21733
gola799 <gandudotcom9955@gmail.com>
2025-01-30 19:27:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask
-
 app = Flask(__name__)
 
-
-@app.route("/")
+@app.route('/')
 def hello_world():
-    return "Jai Shree Krishna !"
+    return 'GrryMatters'
+
+
+if __name__ == "__main__":
+    app.run()
